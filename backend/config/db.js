d00f373a6efe2762561ea92a4f9e4f8a6616c5b3
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-11-24 11:52:52 +0530

    Authentication is part is done and database connected with live database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+
+// const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+// console.log(username, password);
 
 const connection = async ()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/boat-database")
+    await mongoose.connect(`mongodb+srv://tarun7:${password}@imdb.46rj40h.mongodb.net/boat?retryWrites=true&w=majority`)
     console.log("Connection established")
 }
 
