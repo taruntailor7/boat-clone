069b37913c753af876a4df3269554a4fad0c0729
Suraj <surajybtg@gmail.com>
2022-11-27 10:11:00 +0530

    shopByCategoriesDelete

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -2,23 +2,7 @@ import categoryModel from "../models/categories.model.js";
 
 export const getCategories = async (req, res) => {
   try {
-    let {
-      page = 1,
-      _sort = "_id",
-      pageSize = 12,
-      _order = "asc",
-      price_gte = 0,
-      price_lte = Infinity,
-    } = req.query;
-
-    const filters = {
-      $and: [{ price: { $gt: price_gte } }, { price: { $lt: price_lte } }],
-    };
-    let categories = await categoryModel
-      .find(filters)
-      .sort({ [_sort]: _order === "asc" ? 1 : -1 })
-      .limit(pageSize)
-      .skip(pageSize * (page - 1));
+    let categories = await categoryModel.find();
     console.log(categories);
     return res.send({
       status: "success",
--- models/categories.model.js
+++ models/categories.model.js
@@ -1,19 +1,6 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  category: String,
-  rating: Number,
-  reviews: Number,
-  original_price: Number,
-  price: Number,
-  discount: Number,
-  isAvailable: Boolean,
-  isSuperSaver: Boolean,
-  image: Array,
-  color: Array,
-});
+const categorySchema = new mongoose.Schema({});
 
 const categoryModel = mongoose.model("shopbycategories", categorySchema);
 
